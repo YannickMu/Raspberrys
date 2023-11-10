66d51af4a1da014d59b14416e1755c8db160e9a3
Yannick Müller <117479959+YannickMu@users.noreply.github.com>
2023-11-10 10:40:36 +0100

    Update DOCKERONDEBIAN.sh

--- DOCKERONDEBIAN.sh
+++ DOCKERONDEBIAN.sh
@@ -2,7 +2,6 @@
  
  
 # Dieses Skript installiert Docker-Engine sowie Docker-Compose und setzt einige Docker-Conatiner mit Web-Server, DNS-Server, MariaDB-Server etc. auf
-# Dieses Skript ist eine Arbeit von Valentin Binotto und Yannick Mueller waehrend deren Tätigkeit in einem Lernendenprojekt bei Andreas Raschle
 # Dieses Skript wird auf eigene Gefahr des Nutzers ausgeführt und die Autoren übernehmen keinerlei Gewaehr für die Funktionstuechtigkeit und Aktualität dieses Skriptes und übernehmen keinerlei Haftung für jegliche Schaeden oder Unannehmlichkeiten welche durch die Verwendung dieses Skripts entstanden sein könnten.
 # Generell gilt: Fuehre ein Skript nur aus, wenn du den Inhalt des Skripts vollständig verstehst!
  
@@ -16,11 +15,11 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
  
 # Add the repository to Apt sources:
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
  
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
 mkdir /home/$USER/proxy-server-yannick
-docker run --name proxy-server-yannick -v /home/$USER/proxy-server-yannick/siteava:/etc/nginx/sites-available -d --network yokohama-net -p 80:80 --restart always nginx
+docker run --name proxy-server-yannick -v /home/$USER/proxy-server-yannick/siteava:/etc/nginx/sites-available -d --network yannick-net -p 80:80 --restart always nginx
